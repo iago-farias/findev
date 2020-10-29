99a49fd26426637b10ce31a92c8ee9772fac0e63
Iago Farias <iagox38@gmail.com>
2020-10-29 14:24:44 -0300

    Adição da webview do github e alterações na tela de cadastro

--- lib/model/Dev.dart
+++ lib/model/Dev.dart
@@ -1,16 +1,16 @@
 
 class Dev {
-  String nome;
+  String name;
   String email;
   String githubUserName;
   String githubProfilePicUrl;
   String location;
   List<String> techs;
 
-  Dev(this.nome, this.email, this.githubUserName, this.githubProfilePicUrl, this.location, this.techs);
+  Dev(this.name, this.email, this.githubUserName, this.githubProfilePicUrl, this.location, this.techs);
 
   Dev.fromJson(Map<String, dynamic> json) 
-    : nome = json['name'],
+    : name = json['name'],
       email = json['email'],
       githubUserName = json['githubUserName'],
       githubProfilePicUrl = json['githubProfilePicUrl'],
